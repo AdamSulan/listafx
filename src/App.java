41efa2b0687658467c3435e502afaaf78baa5aed
SulanAdam <sulan.adam19@gmail.com>
2023-03-20 16:41:00 +0100

    kezdés

---
+++ src/App.java
@@ -0,0 +1,21 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import views.MainPanel;
+
+public class App extends Application {
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        StackPane stackPane = new StackPane();
+        MainPanel mainPanel=new MainPanel();
+        Scene scene=new Scene(mainPanel,300,400);
+        stage.setScene(scene);
+        stage.show();
+    }
+    
+}
